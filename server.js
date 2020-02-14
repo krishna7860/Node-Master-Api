84355bdfd00b5fc2e504cfd1a1e73e797d194e35
Coder-Hex <rohansharma.8574@gmail.com>
2020-02-15 00:31:03 +0530

    Database Added Model Bootcamp

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const env = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamp");
+const colors = require("colors");
+const connect = require("./config/db");
 
 // Load environment variables
 env.config({ path: "./config/config.env" });
+
+// Connect to database
+connect();
+
+// Route files
+const bootcamps = require("./routes/bootcamp");
 
 const app = express();
 
@@ -18,7 +23,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle Unhandled Rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Unhandled Rejection : ${err.message}`.red);
+  // Close Server
+  server.close(() => process.exit(1));
+});
